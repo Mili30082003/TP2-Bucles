3b6223041254a0bab8c3453baff127d2d74e079d
Mili Antoni <miliantoni12@gmail.com>
2024-07-10 00:01:02 -0300

    Ejercicio 1

---
+++ Ejercicio-1/js/script.js
@@ -0,0 +1,7 @@
+let edad = prompt("Ingrese tu edad");
+if (edad > 18) {
+    document.write ("usted puede conducir");
+    
+} else {
+    document.write ("La edad ingresada no es un numero valido");
+}
