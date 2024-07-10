d3312d61d247094747e2ff7316bc3a04d107f4b8
Mili Antoni <miliantoni12@gmail.com>
2024-07-10 09:52:45 -0300

    Ejercicio 3

---
+++ Ejercicio-3/js/script.js
@@ -0,0 +1,15 @@
+let frase = "";
+do {
+    const palabraIngresada = prompt ("Ingresar una palabra");
+    if (frase.length === 0) {
+        frase = palabraIngresada 
+        
+    }else{
+        frase = frase + '-' + palabraIngresada;
+    }
+    
+} while (confirm('Desea continuar?'));
+document.write ('frase Ingresada: ${frase}');
+
+ 
+
